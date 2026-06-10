ac3b859811eca353dc58feb99e3b7043594868e9
LINGESH036 <lingeshl497@gmail.com>
2026-06-10 12:13:17 +0530

    first com

---
+++ demogit1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demogit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+        }
+    }
+}
